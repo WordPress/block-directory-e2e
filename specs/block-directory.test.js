52d4b5571e0600ffe8e000b4380d26b3ea6895f4
Dion Hulse <contact@dd32.id.au>
2020-07-17 15:47:06 +1000

    Catch JS Parse errors and report that as the error variable instead of a protocol error

--- specs/block-directory.test.js
+++ specs/block-directory.test.js
@@ -37,6 +37,12 @@ const urlMatch = ( url ) => {
 
 const { searchTerm } = github.context.payload.client_payload;
 
+// Variable to hold any encounted JS errors.
+let jsError = false;
+page.on( 'pageerror', error => {
+	jsError = error.toString();
+} );
+
 core.info( `
 --------------------------------------------------------------
 Running Tests for "${ searchTerm }"
@@ -93,8 +99,9 @@ describe( `Block Directory Tests`, () => {
 			core.setOutput( 'success', true );
 			done();
 		} catch ( e ) {
-			core.setFailed( e );
-			core.setOutput( 'error', e.message );
+
+			core.setFailed( e.message );
+			core.setOutput( 'error', jsError || e.message );
 			core.setOutput( 'success', false );
 			done();
 		}
